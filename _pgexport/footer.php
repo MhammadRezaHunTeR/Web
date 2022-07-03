d2f1dc621a6304b084537d2f3b6d2ade5ea56e3f
MhammadRezaHunTeR <mrezasli123@gmail.com>
2022-07-03 16:45:57 +0430

    Learn-math
    This is a my first website on github

---
+++ _pgexport/footer.php
@@ -0,0 +1,11 @@
+
+
+        <?php wp_footer(); ?>
+    </body>
+    <!-- /.container -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+</html>
---
+++ _pgexport/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--  Converted from HTML to WordPress with Pinegrow Web Editor. https://pinegrow.com  -->
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap core CSS -->
+        <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Logo.png" rel="icon">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+        <!-- Custom styles for this template -->
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <meta content="Pinegrow Web Editor" name="generator">
+        <?php wp_head(); ?>
+    </head>
+    <!-- NAVBAR
+================================================== -->
+    <body class="<?php echo implode(' ', get_body_class()); ?>">
+        <?php if( function_exists( 'wp_body_open' ) ) wp_body_open(); ?>
